3379b6c75b9133a5fd16279b1af4710b77d70088
성수 조 <css851028@gmail.com>
2026-01-19 14:39:50 +0900

    data rm

--- publish.cmd
+++
@@ -1,9 +0,0 @@
-dotnet publish .\src\FrontEndWeb\FrontEndWeb.csproj -o ./output/front
-dotnet publish .\src\BattleServer\BattleServer.csproj -o ./output/battle
-dotnet publish .\src\ChatServer\ChatServer.csproj -o ./output/chat
-dotnet publish .\src\GateServer\GateServer.csproj -o ./output/gate
-dotnet publish .\src\WebTool\WebTool.csproj -o ./output/webtool
-dotnet publish .\src\SponsorshipServer\SponsorshipServer.csproj -o ./output/sponsorship
-dotnet publish .\test\TestConsoleApp\TestConsoleApp.csproj -o ./output/stress
-
-xcopy .\src\csvdata .\output\csvdata /Y
--- run.cmd
+++
@@ -1,6 +0,0 @@
-﻿@echo off
-cmd /C start "battle" /D "%CD%\output\battle" dotnet .\BattleServer.dll --ENVIRONMENT=LocalPublishKgh
-cmd /C start "chat" /D "%CD%\output\chat" dotnet .\ChatServer.dll --ENVIRONMENT=LocalPublishKgh
-cmd /C start "front" /D "%CD%\output\front" dotnet .\FrontEndWeb.dll --ENVIRONMENT=LocalPublishKgh
-cmd /C start "webtool" /D "%CD%\output\webtool" dotnet .\WebTool.dll --ENVIRONMENT=LocalPublishKgh
-cmd /C start "gate" /D "%CD%\output\gate" dotnet .\GateServer.dll --ENVIRONMENT=LocalPublishKgh
